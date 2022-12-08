cd0945771040c12265796bd879f6e98f30f7523d
rabbitism <popmessiah@hotmail.com>
2022-12-09 01:14:49 +0800

    feat: update button themes.

--- src/Semi.Avalonia.Demo/Program.cs
+++ src/Semi.Avalonia.Demo/Program.cs
@@ -16,6 +16,7 @@ namespace Semi.Avalonia.Demo
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
+                .With(new Win32PlatformOptions(){ UseCompositor = false})
                 .LogToTrace();
     }
 }
