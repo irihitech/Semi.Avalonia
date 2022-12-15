873c002eadf863c950bcdf94412073cbc4adbb6d
Dong Bin <14807942+rabbitism@users.noreply.github.com>
2022-12-15 16:42:48 +0800

    feat: repeat button default theme. (#8)

---
+++ src/Semi.Avalonia.Demo/Pages/RepeatButtonDemo.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Semi.Avalonia.Demo.Pages;
+
+public partial class RepeatButtonDemo : UserControl
+{
+    public RepeatButtonDemo()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
