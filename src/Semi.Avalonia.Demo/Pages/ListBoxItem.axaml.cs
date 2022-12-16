2e49959dc039173e221a063bc6b0e91c3c02db26
rabbitism <popmessiah@hotmail.com>
2022-12-16 23:39:28 +0800

    feat: add listbox item.

---
+++ src/Semi.Avalonia.Demo/Pages/ListBoxItem.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Semi.Avalonia.Demo.Pages;
+
+public partial class ListBoxItem : UserControl
+{
+    public ListBoxItem()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
