5ba21de6bb30ddde9c60bbc94af1f507c90e007e
rabbitism <popmessiah@hotmail.com>
2023-02-04 14:15:19 +0800

    feat: add ThemeVariant demo.

--- demo/Semi.Avalonia.Demo/Pages/ThemeVariantDemo.axaml.cs
+++ demo/Semi.Avalonia.Demo/Pages/ThemeVariantDemo.axaml.cs
@@ -1,13 +1,30 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
 
 namespace Semi.Avalonia.Demo.Pages;
 
 public partial class ThemeVariantDemo : UserControl
 {
+    private ThemeVariant _variant = ThemeVariant.Default;
     public ThemeVariantDemo()
     {
         InitializeComponent();
+    }
+
+    private void Switch_OnIsCheckedChanged(object sender, RoutedEventArgs e)
+    {
+        if (_variant == ThemeVariant.Dark)
+        {
+            scope.RequestedThemeVariant = ThemeVariant.Default;
+            _variant = ThemeVariant.Default;
+        }
+        else
+        {
+            scope.RequestedThemeVariant = ThemeVariant.Dark;
+            _variant = ThemeVariant.Dark;
+        }
     }
 }
