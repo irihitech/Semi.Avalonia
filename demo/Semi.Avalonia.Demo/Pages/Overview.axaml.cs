8e10cd774ae47fd0828e0bbca99e844fabd4848b
rabbitism <popmessiah@hotmail.com>
2023-02-04 14:00:20 +0800

    feat: Add new index for theme. fix some minor issue.

--- demo/Semi.Avalonia.Demo/Pages/Overview.axaml.cs
+++ demo/Semi.Avalonia.Demo/Pages/Overview.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
 
 namespace Semi.Avalonia.Demo.Pages;
 
@@ -9,5 +11,18 @@ public partial class Overview : UserControl
     public Overview()
     {
         InitializeComponent();
+    }
+
+    private void ToggleButton_OnIsCheckedChanged(object sender, RoutedEventArgs e)
+    {
+        var variant = Application.Current!.RequestedThemeVariant;
+        if (variant?.Key == "Dark")
+        {
+            Application.Current!.RequestedThemeVariant = ThemeVariant.Light;
+        }
+        else
+        {
+            Application.Current!.RequestedThemeVariant = ThemeVariant.Dark;
+        }
     }
 }
