e37f97042e18232877d1a8e62f8b046ac86acc12
rabbitism <popmessiah@hotmail.com>
2023-02-09 22:21:36 +0800

    feat: use vertical navigation tab control.

--- demo/Semi.Avalonia.Demo/Pages/ComboBoxDemo.axaml.cs
+++ demo/Semi.Avalonia.Demo/Pages/ComboBoxDemo.axaml.cs
@@ -10,9 +10,4 @@ public partial class ComboBoxDemo : UserControl
     {
         InitializeComponent();
     }
-
-    private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
-    }
 }
