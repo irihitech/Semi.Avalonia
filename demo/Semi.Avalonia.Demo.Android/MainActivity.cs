0657c99f310b1ecb78b5755266afbec1e66d307c
rabbitism <popmessiah@hotmail.com>
2023-03-20 23:14:50 +0800

    misc: try to fix android app.

--- demo/Semi.Avalonia.Demo.Android/MainActivity.cs
+++ demo/Semi.Avalonia.Demo.Android/MainActivity.cs
@@ -2,10 +2,11 @@ using Android.App;
 using Android.Content.PM;
 using Avalonia.Android;
 
-namespace Semi.Avalonia.Demo;
+namespace Semi.Avalonia.Demo.Android;
 
-[Activity(Label = "Semi.Avalonia.Demo.Android", Icon = "@drawable/Icon",
+[Activity(Label = "Semi.Avalonia.Demo.Android", Icon = "@drawable/Icon", Theme = "@style/MyTheme.NoActionBar",
     LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
 public class MainActivity : AvaloniaMainActivity
 {
+    
 }
