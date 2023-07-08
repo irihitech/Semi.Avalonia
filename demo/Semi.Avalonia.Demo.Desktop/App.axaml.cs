40d179d4b4c5ff64364c67fddfc6e23869fa3cd9
ImLuJian <504439788@qq.com>
2023-07-08 17:57:44 +0800

    添加DRM启动方式，在VM虚拟机上测试了一下Ubuntu18.04 Server，可以启动

--- demo/Semi.Avalonia.Demo.Desktop/App.axaml.cs
+++ demo/Semi.Avalonia.Demo.Desktop/App.axaml.cs
@@ -1,7 +1,6 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Avalonia.Styling;
 using Semi.Avalonia.Demo.Views;
 
 namespace Semi.Avalonia.Demo.Desktop;
@@ -16,9 +15,9 @@ public partial class App : Application
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
             desktop.MainWindow = new MainWindow();
-        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleView)
+            singleView.MainView = new MainView();
         base.OnFrameworkInitializationCompleted();
     }
 }
