b665c0c7e3d7e59fae08cea2d053fb8be4050d5b
rabbitism <popmessiah@hotmail.com>
2023-07-25 23:15:55 +0800

    feat: initialize tree data grid projects.

---
+++ demo/Semi.Avalonia.TreeDataGrid.Demo/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Semi.Avalonia.TreeDataGrid.Demo;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
