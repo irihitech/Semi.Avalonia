430a7e56ca6923c16561bf5c0d5243b9f2d2da30
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2023-08-14 18:49:33 +0800

    feat: ListBox demo.

--- demo/Semi.Avalonia.Demo/Pages/ListBoxDemo.axaml.cs
+++ demo/Semi.Avalonia.Demo/Pages/ListBoxDemo.axaml.cs
@@ -11,18 +11,5 @@ public partial class ListBoxDemo : UserControl
         InitializeComponent();
     }
 
-    public IEnumerable Items { get; set; } = new List<Panda>
-    {
-        new() { Name = "Ding", IsAvailable = true },
-        new() { Name = "Otter", IsAvailable = true },
-        new() { Name = "Husky", IsAvailable = false },
-        new() { Name = "Mr.17", IsAvailable = true },
-        new() { Name = "Cass", IsAvailable = true },
-    };
-}
-
-internal record Panda
-{
-    public string Name { get; set; }
-    public bool IsAvailable { get; set; }
+    public IEnumerable Items { get; set; } = new List<string> { "Ding", "Otter", "Husky", "Mr.17", "Cass", };
 }
