e1588120f25a5309aaf4a9b6113728e51aca28a8
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2023-08-14 10:49:06 +0800

    feat: RadioGroupListBox theme.

--- demo/Semi.Avalonia.Demo/Pages/ListBoxDemo.axaml.cs
+++ demo/Semi.Avalonia.Demo/Pages/ListBoxDemo.axaml.cs
@@ -1,6 +1,6 @@
-using Avalonia;
+using System.Collections;
+using System.Collections.Generic;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
 namespace Semi.Avalonia.Demo.Pages;
 
@@ -10,4 +10,19 @@ public partial class ListBoxDemo : UserControl
     {
         InitializeComponent();
     }
+
+    public IEnumerable Items { get; set; } = new List<Panda>
+    {
+        new() { Name = "Ding", IsAvailable = true },
+        new() { Name = "Otter", IsAvailable = true },
+        new() { Name = "Husky", IsAvailable = false },
+        new() { Name = "Mr.17", IsAvailable = true },
+        new() { Name = "Cass", IsAvailable = true },
+    };
+}
+
+internal record Panda
+{
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; }
 }
