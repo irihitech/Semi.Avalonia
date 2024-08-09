5161331b98985e871879f6ea5c9f1b6d31de20c9
Gehongyan <gehongyan1996@126.com>
2024-08-10 00:25:02 +0800

    feat: ja-jp localization
    (cherry picked from commit f564c98afdd961768c9975dd7124810c43129276)

--- src/Semi.Avalonia/Themes/SemiTheme.axaml.cs
+++ src/Semi.Avalonia/Themes/SemiTheme.axaml.cs
@@ -14,6 +14,7 @@ public class SemiTheme : Styles
     {
         { new CultureInfo("zh-cn"), new zh_cn() },
         { new CultureInfo("en-us"), new en_us() },
+        { new CultureInfo("ja-jp"), new ja_jp() },
         { new CultureInfo("ru-ru"), new ru_ru() },
     };
 
