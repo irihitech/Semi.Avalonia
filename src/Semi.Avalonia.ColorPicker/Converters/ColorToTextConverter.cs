9b9c05773b2c3f95344627f39af342f9ca1c2d1e
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2024-08-13 14:45:10 +0800

    fix: fix issues.

--- src/Semi.Avalonia.ColorPicker/Converters/ColorToTextConverter.cs
+++ src/Semi.Avalonia.ColorPicker/Converters/ColorToTextConverter.cs
@@ -12,20 +12,21 @@ public class ColorToTextConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        return value is Color color ? $"{color.R},{color.G},{color.B}" : AvaloniaProperty.UnsetValue;
+        return value is Color color ? $"{color.R},{color.G},{color.B},{color.A}" : AvaloniaProperty.UnsetValue;
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is not string str) return BindingOperations.DoNothing;
         var parts = str.Split(',');
-        if (parts.Length != 3 || parts.Any(string.IsNullOrWhiteSpace)) return BindingOperations.DoNothing;
+        if (parts.Length != 4 || parts.Any(string.IsNullOrWhiteSpace)) return BindingOperations.DoNothing;
 
         if (byte.TryParse(parts[0], NumberStyles.Integer, CultureInfo.InvariantCulture, out var r) &&
             byte.TryParse(parts[1], NumberStyles.Integer, CultureInfo.InvariantCulture, out var g) &&
-            byte.TryParse(parts[2], NumberStyles.Integer, CultureInfo.InvariantCulture, out var b))
+            byte.TryParse(parts[2], NumberStyles.Integer, CultureInfo.InvariantCulture, out var b) &&
+            byte.TryParse(parts[3], NumberStyles.Integer, CultureInfo.InvariantCulture, out var a))
         {
-            return new Color(0xFF, r, g, b);
+            return new Color(a, r, g, b);
         }
 
         return BindingOperations.DoNothing;
--- src/Semi.Avalonia.ColorPicker/Converters/HsvColorToTextConverter.cs
+++ src/Semi.Avalonia.ColorPicker/Converters/HsvColorToTextConverter.cs
@@ -13,7 +13,7 @@ public class HsvColorToTextConverter : IValueConverter
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return value is HsvColor hsvColor
-            ? $"{Math.Round(hsvColor.H)},{Math.Round(hsvColor.S * 100)},{Math.Round(hsvColor.V * 100)}"
+            ? $"{Math.Round(hsvColor.H)},{Math.Round(hsvColor.S * 100)},{Math.Round(hsvColor.V * 100)},{Math.Round(hsvColor.A * 100)}"
             : AvaloniaProperty.UnsetValue;
     }
 
@@ -21,13 +21,14 @@ public class HsvColorToTextConverter : IValueConverter
     {
         if (value is not string str) return BindingOperations.DoNothing;
         var parts = str.Split(',');
-        if (parts.Length != 3 || parts.Any(string.IsNullOrWhiteSpace)) return BindingOperations.DoNothing;
+        if (parts.Length != 4 || parts.Any(string.IsNullOrWhiteSpace)) return BindingOperations.DoNothing;
 
         if (double.TryParse(parts[0], NumberStyles.Float, CultureInfo.InvariantCulture, out var h) &&
             double.TryParse(parts[1], NumberStyles.Float, CultureInfo.InvariantCulture, out var s) &&
-            double.TryParse(parts[2], NumberStyles.Float, CultureInfo.InvariantCulture, out var v))
+            double.TryParse(parts[2], NumberStyles.Float, CultureInfo.InvariantCulture, out var v) &&
+            double.TryParse(parts[3], NumberStyles.Float, CultureInfo.InvariantCulture, out var a))
         {
-            return new HsvColor(1, h, s / 100, v / 100);
+            return new HsvColor(a / 100, h, s / 100, v / 100);
         }
 
         return BindingOperations.DoNothing;
