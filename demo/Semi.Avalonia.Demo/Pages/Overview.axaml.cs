99d928ea15220b2d3350b8643d1ac34c89219a82
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2024-11-15 20:59:46 +0800

    feat: add pack&publish scripts to workflow.

--- demo/Semi.Avalonia.Demo/Pages/Overview.axaml.cs
+++ demo/Semi.Avalonia.Demo/Pages/Overview.axaml.cs
@@ -9,7 +9,7 @@ public partial class Overview : UserControl
         InitializeComponent();
     }
 
-    public string MainInstall { get; set; } = "dotnet add package Semi.Avalonia --version 11.1.0";
+    public string MainInstall { get; set; } = "dotnet add package Semi.Avalonia --version 11.2.1";
 
     public string MainStyle { get; set; } =
         """
@@ -20,7 +20,7 @@ public partial class Overview : UserControl
         </Application.Styles>
         """;
 
-    public string ColorPickerInstall { get; set; } = "dotnet add package Semi.Avalonia.ColorPicker --version 11.1.0";
+    public string ColorPickerInstall { get; set; } = "dotnet add package Semi.Avalonia.ColorPicker --version 11.2.1";
 
     public string ColorPickerStyle { get; set; } =
         """
@@ -29,7 +29,7 @@ public partial class Overview : UserControl
         </Application.Styles>
         """;
 
-    public string DataGridInstall { get; set; } = "dotnet add package Semi.Avalonia.DataGrid --version 11.1.0";
+    public string DataGridInstall { get; set; } = "dotnet add package Semi.Avalonia.DataGrid --version 11.2.1";
 
     public string DataGridStyle { get; set; } =
         """
@@ -38,7 +38,7 @@ public partial class Overview : UserControl
         </Application.Styles>
         """;
 
-    public string TreeDataGridInstall { get; set; } = "dotnet add package Semi.Avalonia.TreeDataGrid --version 11.1.0";
+    public string TreeDataGridInstall { get; set; } = "dotnet add package Semi.Avalonia.TreeDataGrid --version 11.0.10.1";
 
     public string TreeDataGridStyle { get; set; } =
         """
