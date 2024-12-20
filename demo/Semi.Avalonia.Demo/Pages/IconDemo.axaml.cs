e61c07f3525e4868f02a2fa66e3c54a008544102
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2024-12-20 23:38:53 +0800

    feat: enhance IconDemo.

--- demo/Semi.Avalonia.Demo/Pages/IconDemo.axaml.cs
+++ demo/Semi.Avalonia.Demo/Pages/IconDemo.axaml.cs
@@ -1,5 +1,9 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
 using Avalonia.Controls.Primitives;
+using Avalonia.Input.Platform;
+using Avalonia.Interactivity;
 using Avalonia.Threading;
 using Semi.Avalonia.Demo.ViewModels;
 
@@ -7,6 +11,9 @@ namespace Semi.Avalonia.Demo.Pages;
 
 public partial class IconDemo : UserControl
 {
+    private IClipboard? _clipboard;
+    private WindowNotificationManager? _windowNotificationManager;
+
     public IconDemo()
     {
         InitializeComponent();
@@ -18,5 +25,23 @@ public partial class IconDemo : UserControl
         base.OnApplyTemplate(e);
         var vm = this.DataContext as IconDemoViewModel;
         await Dispatcher.UIThread.InvokeAsync(() => { vm?.InitializeResources(); });
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        var topLevel = TopLevel.GetTopLevel(this);
+        _windowNotificationManager = new WindowNotificationManager(topLevel) { MaxItems = 3 };
+        _clipboard = topLevel?.Clipboard;
+    }
+
+    private async void Button_Clicked(object sender, RoutedEventArgs e)
+    {
+        if (_clipboard is null) return;
+        if (sender is not Button { DataContext: IconItem s }) return;
+        await _clipboard.SetTextAsync(s.ResourceKey);
+        _windowNotificationManager?.Show(
+            new Notification("Copied", s.ResourceKey),
+            NotificationType.Success);
     }
 }
--- demo/Semi.Avalonia.Demo/ViewModels/IconDemoViewModel.cs
+++ demo/Semi.Avalonia.Demo/ViewModels/IconDemoViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
@@ -12,24 +14,51 @@ public partial class IconDemoViewModel : ObservableObject
     private readonly IResourceDictionary? _resources =
         AvaloniaXamlLoader.Load(new Uri("avares://Semi.Avalonia/Themes/Shared/Icon.axaml")) as ResourceDictionary;
 
-    public ObservableCollection<IconItem> Icons { get; set; } = [];
+    private readonly Dictionary<string, IconItem> _filledIcons = new();
+    private readonly Dictionary<string, IconItem> _strokedIcons = new();
+
+    [ObservableProperty] private string? _searchText;
+
+    public ObservableCollection<IconItem> FilteredFilledIcons { get; set; } = [];
+    public ObservableCollection<IconItem> FilteredStrokedIcons { get; set; } = [];
 
     public void InitializeResources()
-    {
-        LoadIcons();
-    }
-
-    private void LoadIcons()
     {
         if (_resources is null) return;
 
         foreach (var key in _resources.Keys)
         {
-            if (_resources[key] is Geometry geometry)
+            if (_resources[key] is not Geometry geometry) continue;
+            var icon = new IconItem { ResourceKey = key.ToString(), Geometry = geometry };
+
+            if (key.ToString().EndsWith("Stroked"))
             {
-                var icon = new IconItem { ResourceKey = key.ToString(), Geometry = geometry };
-                Icons.Add(icon);
+                _strokedIcons[key.ToString()] = icon;
             }
+            else
+            {
+                _filledIcons[key.ToString()] = icon;
+            }
+
+            OnSearchTextChanged(string.Empty);
+        }
+    }
+
+    partial void OnSearchTextChanged(string? value)
+    {
+        var search = value?.ToLowerInvariant() ?? string.Empty;
+
+        FilteredFilledIcons.Clear();
+        foreach (var icon in _filledIcons.Values.Where(i => i.ResourceKey?.ToLowerInvariant().Contains(search) == true))
+        {
+            FilteredFilledIcons.Add(icon);
+        }
+
+        FilteredStrokedIcons.Clear();
+        foreach (var icon in
+                 _strokedIcons.Values.Where(i => i.ResourceKey?.ToLowerInvariant().Contains(search) == true))
+        {
+            FilteredStrokedIcons.Add(icon);
         }
     }
 }
