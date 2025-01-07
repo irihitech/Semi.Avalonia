ea51867f0f6c4fe09251ab12f6bd3ddd8040962f
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-01-07 12:41:51 +0800

    feat: add RegisterFollowSystemTheme extension.

--- demo/Semi.Avalonia.Demo/App.axaml.cs
+++ demo/Semi.Avalonia.Demo/App.axaml.cs
@@ -1,10 +1,7 @@
-using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
-using Avalonia.Media;
-using Avalonia.Platform;
 using Semi.Avalonia.Demo.Views;
 
 namespace Semi.Avalonia.Demo;
@@ -31,30 +28,7 @@ public partial class App : Application
                 break;
         }
 
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            var setting = PlatformSettings?.GetColorValues();
-            if (setting?.ContrastPreference is ColorContrastPreference.High)
-            {
-                if (setting.AccentColor1 == Color.Parse("#8EE3F0"))
-                {
-                    RequestedThemeVariant = SemiTheme.Aquatic;
-                }
-                else if (setting.AccentColor1 == Color.Parse("#903909"))
-                {
-                    RequestedThemeVariant = SemiTheme.Desert;
-                }
-                else if (setting.AccentColor1 == Color.Parse("#A1BFDE"))
-                {
-                    RequestedThemeVariant = SemiTheme.Dusk;
-                }
-                else if (setting.AccentColor1 == Color.Parse("#D6B4FD"))
-                {
-                    RequestedThemeVariant = SemiTheme.NightSky;
-                }
-            }
-        }
-
+        this.RegisterFollowSystemTheme();
         base.OnFrameworkInitializationCompleted();
     }
 }
--- demo/Semi.Avalonia.Demo/Views/MainView.axaml.cs
+++ demo/Semi.Avalonia.Demo/Views/MainView.axaml.cs
@@ -33,9 +33,14 @@ public partial class MainViewModel : ObservableObject
         [
             new MenuItemViewModel
             {
-                Header = "High Contrast Theme",
+                Header = "Theme",
                 Items =
                 [
+                    new MenuItemViewModel
+                    {
+                        Header = "Auto",
+                        Command = FollowSystemThemeCommand
+                    },
                     new MenuItemViewModel
                     {
                         Header = "Aquatic",
@@ -62,10 +67,10 @@ public partial class MainViewModel : ObservableObject
                     },
                 ]
             },
-            new MenuItemViewModel()
+            new MenuItemViewModel
             {
                 Header = "Locale",
-                Items=
+                Items =
                 [
                     new MenuItemViewModel
                     {
@@ -115,32 +120,36 @@ public partial class MainViewModel : ObservableObject
     }
 
     [RelayCommand]
+    private void FollowSystemTheme()
+    {
+        Application.Current?.RegisterFollowSystemTheme();
+    }
+
+    [RelayCommand]
     private void ToggleTheme()
     {
         var app = Application.Current;
         if (app is null) return;
         var theme = app.ActualThemeVariant;
         app.RequestedThemeVariant = theme == ThemeVariant.Dark ? ThemeVariant.Light : ThemeVariant.Dark;
+        app.UnregisterFollowSystemTheme();
     }
 
     [RelayCommand]
     private void SelectTheme(object? obj)
     {
         var app = Application.Current;
-        if (app is not null)
-        {
-            app.RequestedThemeVariant = obj as ThemeVariant;
-        }
+        if (app is null) return;
+        app.RequestedThemeVariant = obj as ThemeVariant;
+        app.UnregisterFollowSystemTheme();
     }
-    
+
     [RelayCommand]
     private void SelectLocale(object? obj)
     {
         var app = Application.Current;
-        if (app is not null)
-        {
-            SemiTheme.OverrideLocaleResources(app, obj as System.Globalization.CultureInfo);
-        }
+        if (app is null) return;
+        SemiTheme.OverrideLocaleResources(app, obj as CultureInfo);
     }
 
     [RelayCommand]
