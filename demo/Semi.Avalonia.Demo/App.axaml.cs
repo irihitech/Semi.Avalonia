70ddd528d62b81a145fc07df2b46f467405d598c
Dong Bin <popmessiah@hotmail.com>
2025-01-11 16:24:36 +0800

    feat: add tray demo, change icon.

--- demo/Semi.Avalonia.Demo/App.axaml.cs
+++ demo/Semi.Avalonia.Demo/App.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using Semi.Avalonia.Demo.ViewModels;
 using Semi.Avalonia.Demo.Views;
 
 namespace Semi.Avalonia.Demo;
@@ -11,6 +12,7 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        this.DataContext = new ApplicationViewModel();
     }
 
     public override void OnFrameworkInitializationCompleted()
