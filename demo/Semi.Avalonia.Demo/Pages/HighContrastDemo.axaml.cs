b5fe4d92a61d9ea8ef285fedce3e6eebe9ce117d
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-01-21 21:17:20 +0800

    feat: add CopyText to HighContrastDemo.

--- demo/Semi.Avalonia.Demo/Pages/HighContrastDemo.axaml.cs
+++ demo/Semi.Avalonia.Demo/Pages/HighContrastDemo.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using System.Threading.Tasks;
+using Avalonia.Controls;
 using Semi.Avalonia.Demo.ViewModels;
 
 namespace Semi.Avalonia.Demo.Pages;
@@ -9,5 +10,15 @@ public partial class HighContrastDemo : UserControl
     {
         InitializeComponent();
         this.DataContext = new HighContrastDemoViewModel();
+    }
+
+    public async Task Copy(object? o)
+    {
+        if (o is null) return;
+        var toplevel = TopLevel.GetTopLevel(this);
+        if (toplevel?.Clipboard is { } c)
+        {
+            await c.SetTextAsync(o.ToString());
+        }
     }
 }
