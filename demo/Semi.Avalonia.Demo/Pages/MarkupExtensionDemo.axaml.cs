aef82456a2c4da84dfc212df11bc1faa03e0baef
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-04-02 18:00:23 +0800

    feat: add MarkupExtension demo.

---
+++ demo/Semi.Avalonia.Demo/Pages/MarkupExtensionDemo.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Semi.Avalonia.Demo.Pages;
+
+public partial class MarkupExtensionDemo : UserControl
+{
+    public MarkupExtensionDemo()
+    {
+        InitializeComponent();
+    }
+}
