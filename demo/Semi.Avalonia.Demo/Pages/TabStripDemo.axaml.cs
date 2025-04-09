28a1336413145f8894a63e0929b4da19289d6c05
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-04-09 17:15:20 +0800

    feat: add TabStripDemo.

---
+++ demo/Semi.Avalonia.Demo/Pages/TabStripDemo.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Semi.Avalonia.Demo.ViewModels;
+
+namespace Semi.Avalonia.Demo.Pages;
+
+public partial class TabStripDemo : UserControl
+{
+    public TabStripDemo()
+    {
+        InitializeComponent();
+        this.DataContext = new TabStripDemoViewModel();
+    }
+}
