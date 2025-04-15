db24d4393b1b4db3cb5a2f5bdedba26c3a2fecf5
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-04-15 20:05:54 +0800

    misc: transfer TreeDataGrid demo.

---
+++ demo/Semi.Avalonia.Demo/Pages/TreeDataGridDemo.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Semi.Avalonia.Demo.ViewModels;
+
+namespace Semi.Avalonia.Demo.Pages;
+
+public partial class TreeDataGridDemo : UserControl
+{
+    public TreeDataGridDemo()
+    {
+        InitializeComponent();
+        this.DataContext = new TreeDataGridDemoViewModel();
+    }
+
+    private void SelectedPath_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            var vm = DataContext as TreeDataGridDemoViewModel;
+            vm.FilesContext.SelectedPath = (sender as TextBox)!.Text;
+        }
+    }
+}
