cae26f2134be655467f7bff7aa156668a8dc2f02
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-04-16 22:09:11 +0800

    misc: format codes.

--- src/Semi.Avalonia/Converters/TreeViewItemIndentConverter.cs
+++ src/Semi.Avalonia/Converters/TreeViewItemIndentConverter.cs
@@ -6,20 +6,17 @@ using Avalonia.Data.Converters;
 
 namespace Semi.Avalonia.Converters;
 
-public class TreeViewItemIndentConverter: IMultiValueConverter
+public class TreeViewItemIndentConverter : IMultiValueConverter
 {
     public static readonly TreeViewItemIndentConverter Instance = new();
-    
+
     public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (values.Count>1 && values[0] is int level && values[1] is double indent)
+        if (values.Count > 1 && values[0] is int level && values[1] is double indent)
         {
-            return new Thickness( 
-                indent * level,
-                0,
-                0,
-                0);
+            return new Thickness(indent * level, 0, 0, 0);
         }
+
         return new Thickness(0);
     }
 }
