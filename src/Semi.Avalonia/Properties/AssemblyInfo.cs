4d439ee87540f08e72adc595a072e20cd2cf9a34
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-09-19 15:14:29 +0800

    misc: update XML namespace references and converters in XAML files.

--- src/Semi.Avalonia/Properties/AssemblyInfo.cs
+++ src/Semi.Avalonia/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Metadata;
 
-[assembly:XmlnsPrefix("https://irihi.tech/semi", "semi")]
-[assembly:XmlnsDefinition("https://irihi.tech/semi", "Semi.Avalonia")]
+[assembly: XmlnsPrefix("https://irihi.tech/semi", "semi")]
+[assembly: XmlnsDefinition("https://irihi.tech/semi", "Semi.Avalonia")]
+[assembly: XmlnsDefinition("https://irihi.tech/semi", "Semi.Avalonia.Converters")]
