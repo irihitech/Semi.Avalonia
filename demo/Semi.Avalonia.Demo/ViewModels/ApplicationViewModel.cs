9dd846f6be68bfb801c02ccfde57a041d021cb3d
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-09-28 18:02:09 +0800

    feat: add JumpTo functionality in ApplicationViewModel and integrate with menu.

--- demo/Semi.Avalonia.Demo/ViewModels/ApplicationViewModel.cs
+++ demo/Semi.Avalonia.Demo/ViewModels/ApplicationViewModel.cs
@@ -2,11 +2,18 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace Semi.Avalonia.Demo.ViewModels;
 
 public partial class ApplicationViewModel : ObservableObject
 {
+    [RelayCommand]
+    private void JumpTo(string header)
+    {
+        WeakReferenceMessenger.Default.Send(header, "JumpTo");
+    }
+
     [RelayCommand]
     private void Exit()
     {
