d865f2aeb49f4b958b54e06745ad27de414778a7
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-03-10 19:07:27 +0800

    chore: enable developer tools in debug mode and update diagnostics package references.

--- demo/Semi.Avalonia.Demo/App.axaml.cs
+++ demo/Semi.Avalonia.Demo/App.axaml.cs
@@ -12,6 +12,9 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+#if DEBUG
+        this.AttachDeveloperTools();
+#endif
         this.DataContext = new ApplicationViewModel();
     }
 
