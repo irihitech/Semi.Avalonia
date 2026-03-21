b3b74590880fb2fa38d61e6736308978f0df7f55
Dong Bin <14807942+rabbitism@users.noreply.github.com>
2026-03-21 17:58:35 +0800

    Pr3/carousel page (#772)
    * feat: add CarouselPage and PipsPager.
    
    * feat: add PipsPager control and integrate into index
    
    * feat: add PipsPagerDemo and integrate into MainView
    
    ---------
    
    Co-authored-by: Zhang Dian <54255897+zdpcdt@users.noreply.github.com>

---
+++ demo/Semi.Avalonia.Demo/Pages/PipsPagerDemo.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+
+namespace Semi.Avalonia.Demo.Pages;
+
+public partial class PipsPagerDemo : UserControl
+{
+    public PipsPagerDemo()
+    {
+        InitializeComponent();
+    }
+}
+
