6fc6e3000f18d4f7a11952a180da49b4d1ba9750
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-04-24 18:24:57 +0800

    feat: add customizations for DrawerPageDemo.

--- demo/Semi.Avalonia.Demo/Pages/DrawerPageDemo.axaml.cs
+++ demo/Semi.Avalonia.Demo/Pages/DrawerPageDemo.axaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Input.GestureRecognizers;
-using Avalonia.Interactivity;
 using Avalonia.Layout;
 
 namespace Semi.Avalonia.Demo.Pages;
@@ -15,32 +13,6 @@ public partial class DrawerPageDemo : UserControl
         EnableMouseSwipeGesture(DemoDrawer);
     }
 
-    protected override void OnLoaded(RoutedEventArgs e)
-    {
-        base.OnLoaded(e);
-        DemoDrawer.Opened += OnDrawerStatusChanged;
-        DemoDrawer.Closed += OnDrawerStatusChanged;
-    }
-
-    protected override void OnUnloaded(RoutedEventArgs e)
-    {
-        base.OnUnloaded(e);
-        DemoDrawer.Opened -= OnDrawerStatusChanged;
-        DemoDrawer.Closed -= OnDrawerStatusChanged;
-    }
-
-    private void OnDrawerStatusChanged(object? sender, EventArgs e) => UpdateStatus();
-
-    private void OnToggleDrawer(object? sender, RoutedEventArgs e)
-    {
-        DemoDrawer.IsOpen = !DemoDrawer.IsOpen;
-    }
-
-    private void OnGestureChanged(object? sender, RoutedEventArgs e)
-    {
-        DemoDrawer.IsGestureEnabled = GestureCheck.IsChecked == true;
-    }
-
     private void OnMenuSelectionChanged(object? sender, SelectionChangedEventArgs e)
     {
         if (DrawerMenu.SelectedItem is ListBoxItem item)
@@ -50,21 +22,13 @@ public partial class DrawerPageDemo : UserControl
                 Header = item.Content?.ToString(),
                 Content = new TextBlock
                 {
-                    Text = $"{item.Content} page content",
-                    FontSize = 16,
-                    HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center,
+                    Text = $"{item.Content} page content", FontSize = 16, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center,
                 },
                 HorizontalContentAlignment = HorizontalAlignment.Stretch,
                 VerticalContentAlignment = VerticalAlignment.Stretch
             };
             DemoDrawer.IsOpen = false;
         }
-    }
-
-    private void UpdateStatus()
-    {
-        StatusText.Text = $"Drawer: {(DemoDrawer.IsOpen ? "Open" : "Closed")}";
     }
 
     private static void EnableMouseSwipeGesture(Control control)
@@ -74,17 +38,5 @@ public partial class DrawerPageDemo : UserControl
             .FirstOrDefault();
 
         recognizer?.IsMouseEnabled = true;
-    }
-
-    private void OnLayoutChanged(object? sender, SelectionChangedEventArgs e)
-    {
-        DemoDrawer.DrawerLayoutBehavior = (sender as ComboBox)?.SelectedIndex switch
-        {
-            0 => DrawerLayoutBehavior.CompactOverlay,
-            1 => DrawerLayoutBehavior.CompactInline,
-            2 => DrawerLayoutBehavior.Split,
-            3 => DrawerLayoutBehavior.Overlay,
-            _ => DrawerLayoutBehavior.CompactOverlay
-        };
     }
 }
