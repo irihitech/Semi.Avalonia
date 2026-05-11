ef9c104122f68f939972fba555d180f396481595
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-05-11 16:14:48 +0800

    feat: add linux options.

--- demo/Semi.Avalonia.Demo.Desktop/Program.cs
+++ demo/Semi.Avalonia.Demo.Desktop/Program.cs
@@ -5,7 +5,7 @@ using Avalonia.Media;
 using Semi.Avalonia.Demo.Fonts;
 
 namespace Semi.Avalonia.Demo.Desktop;
-#pragma warning disable AVALONIA_X11_CSD, AVALONIA_X11_FORCE_CSD
+#pragma warning disable CA1416, AVALONIA_X11_CSD, AVALONIA_X11_FORCE_CSD
 
 sealed class Program
 {
@@ -13,16 +13,28 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        var appBuilder = BuildAvaloniaApp();
+        if (!OperatingSystem.IsLinux())
+            appBuilder.With(new FontManagerOptions
+            {
+                FontFallbacks = [new FontFallback { FontFamily = new FontFamily("Microsoft YaHei") }]
+            });
+
+        appBuilder.StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        var appBuilder = AppBuilder.Configure<App>()
             .UseManagedSystemDialogs()
             .UsePlatformDetect()
             .With(new Win32PlatformOptions())
-            .With(new X11PlatformOptions { EnableDrawnDecorations = true, ForceDrawnDecorations = true })
-            .WithSourceHanSansCNFont()
+            .With(new X11PlatformOptions { EnableDrawnDecorations = true })
             .LogToTrace();
+        if (OperatingSystem.IsLinux()) appBuilder.WithSourceHanSansCNFont();
+        return appBuilder;
+    }
 }
